bd10122ef98b456bcb824c14b1cd8aa89d410f21
Sharmin <thelordcloud@gmail.com>
2020-12-07 08:17:14 +0200

    description changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    'I am a graphic designer turned software developer. I am eager to apply programming and development knowledge acquired from the software development vocational training into practicality.'}
                 </p>
                 <p className="about-wrapper__info-text">{paragraphTwo || null}</p>
                 <p className="about-wrapper__info-text">{paragraphThree || null}</p>
